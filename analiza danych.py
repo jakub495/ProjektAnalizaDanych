c763326a4d6f61780178453ec669d44abf60c7b8
wengorz997 <jakub.kuta1995@gmail.com>
2026-02-23 20:05:35 +0100

    update

--- analiza danych.py
+++ analiza danych.py
@@ -2,6 +2,36 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
+# Bydowanie wykresu
+def rysuj_wykres_ciagly(df):
+    if df is not None:
+        dane_ciagle = df.values.flatten()
+
+        plt.figure(figsize=(15, 5))
+
+        # 2. Rysujemy jedną linię
+        plt.plot(dane_ciagle, marker='o', linestyle='-', color='darkcyan', markersize=4)
+
+        # 3. Dodajemy opisy
+        plt.title('Ciągły przebieg wszystkich pomiarów (kolumna po kolumnie)')
+        plt.xlabel('Całkowita liczba punktów pomiarowych')
+        plt.ylabel('Wartość')
+        plt.grid(True, alpha=0.3)
+
+        # Pionowe linie oddzielające dane z różnych kolumn
+        liczba_wierszy = len(df)
+        for i in range(1, len(df.columns)):
+            plt.axvline(x=i * liczba_wierszy, color='red', linestyle='--', alpha=0.5)
+
+        plt.show()
+    else:
+        print("Błąd: Brak danych!")
+
+
+
+
 def menu_glowne():
     df = None
 
@@ -9,7 +39,8 @@ def menu_glowne():
         print("\n Analiza NT-proBNP")
         print("1. Wczytaj plik (CSV/JSON)")
         print("2. Pokaż podsumowanie danych")
-        print("3. Wyświetl statystyki")
+        print("3. Wyświetl statystyki") #do zrobienia
+        print("4. Generuj wykres")
         print("0. Wyjście")
 
         wybor = input("Wybierz opcję: ")
@@ -18,10 +49,10 @@ def menu_glowne():
             sciezka = input("Podaj nazwę pliku: ")
             try:
                 if sciezka.endswith('.csv'):
-                    df = pd.read_csv(sciezka)
+                    df = pd.read_csv(sciezka, header=None)
                     print("CSV wczytany pomyślnie!")
                 elif sciezka.endswith('.json'):
-                    df = pd.read_json(sciezka)
+                    df = pd.read_json(sciezka, header=None)
                     print("JSON wczytany pomyślnie!")
                 else:
                     print("Błąd: Obsługujemy tylko .csv i .json")
@@ -36,10 +67,13 @@ def menu_glowne():
 
         elif wybor == '3':
             if df is not None:
-                # pandas.describe() korzysta z numpy do obliczeń
+                # pandas.describe() korzysta z numpy do obliczeń/ zamienić na numpy + dodać komunikat o anomaliach
                 print(df.describe())
             else:
                 print("Błąd: Brak danych do analizy!")
+
+        elif wybor == '4':
+            rysuj_wykres_ciagly(df)
 
         elif wybor == '0':
             print("Do widzenia.")
